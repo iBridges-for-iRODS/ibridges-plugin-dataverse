9f1a6ffe08216139c210685db800e3863011c2a7
chstaiger <staigerchristine@gmail.com>
2025-07-09 11:08:04 +0200

    rename file to match command

--- ibridgescontrib/ibridgesdvn/__init__.py
+++ ibridgescontrib/ibridgesdvn/__init__.py
@@ -1,4 +1,4 @@
-from ibridgescontrib.ibridgesdvn.dvn_alias import CliDvnAlias
+from ibridgescontrib.ibridgesdvn.dvn_setup import CliDvnAlias
 from ibridgescontrib.ibridgesdvn.dvn_init import CliDvnInit
 
 SETUP_COMMANDS = [CliDvnAlias]
