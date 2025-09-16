e7c1fe61a265d2b049a5b5b7eb715c66bb509086
Staiger, Christine <staigerchristine@gmail.com>
2025-09-16 04:35:47 +0200

    ruff and pylint

--- ibridgescontrib/ibridgesdvn/__init__.py
+++ ibridgescontrib/ibridgesdvn/__init__.py
@@ -23,10 +23,13 @@ CREATE_DS_COMMANDS = [CliDvnCreateDataset]
 
 __all__ = ["get_dataverse_tab"]
 
+# pylint: disable=import-outside-toplevel
 def get_dataverse_tab():
     """Import for GUI."""
     try:
-        from ibridgescontrib.ibridgesdvn.gui.gui_dataverse import DataverseTab # pylint: disable=import-outside-toplevel
+        from ibridgescontrib.ibridgesdvn.gui.gui_dataverse import (
+            DataverseTab,
+        )
     except ImportError as e:
         raise ImportError(
             "GUI dependencies missing. Install with:\n"
